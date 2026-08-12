44e9b714dbc13117036a05c6c4c7b69db2aa4c40
rafaelrdasilva2020-coder <rafaelrdasilva2020@gmail.com>
2026-08-12 09:09:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // aparelhos dos alunos a buscarem os arquivos novos em vez de
 // continuarem servindo a versão antiga do cache.
 // =========================================================
-const CACHE_VERSION = "kyvenza-v14";
+const CACHE_VERSION = "kyvenza-v15";
 
 const ARQUIVOS_ESTATICOS = [
   "index.html",
